7f096fec373cc785751a0a841c75680a9de7618b
Megan McKeough <megan.mckeough@gmail.com>
2017-01-24 21:02:33 +1100

    Upload 24 Jan
    Initial code

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	console.log("ready");
+	$("#eternalButton").click(function () {
+		alert("Sorry, Lacuna Inc. isn't quite ready yet... Coming soon!");
+	});
+	$("#prestigeButton").click(function () {
+		alert("Sorry, Tesla is fixing this glitch... Not quite ready!");
+	});
+});
+	
+
+	
+	
